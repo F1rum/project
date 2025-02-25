dfb306e775e70b749c076af7fba9f7cfe1e8f62f
nomercyzzz <ghostnomercy007@gmail.com>
2025-02-25 03:57:57 +0300

    смена библиотеки при написании бота с node-telegram-bot-api на Telegraf; добавление кнопок;  обработка нажатий на кнопку; команда start menu help

--- bot_development/bot_test.js
+++ bot_development/bot_test.js
@@ -1,15 +1,70 @@
 require('dotenv').config();
-const TelegramBot = require('node-telegram-bot-api');
+const { Telegraf } = require('telegraf');
 
-const token = process.env.TELEGRAM_BOT_TOKEN;
-const bot = new TelegramBot(token, { polling: true });
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Hello, World!');
+bot.help((ctx)=>{
+  ctx.reply('Если тебе нужна помощь, напиши мне <b>@nomercyforthemost</b>', { parse_mode: 'HTML' });
 });
 
-process.on('SIGINT', () => {            // Остановка бота при завершении процесса при сочетании клавиш Ctrl+C
-  bot.stopPolling();
-  process.exit();
+bot.start((ctx) =>{
+  ctx.replyWithSticker('https://cdn2.combot.org/programmingsuicaplusplus/webp/16xf09f9396.webp');
+  ctx.reply('Привет! 👋 Я — твой помощник по скидкам. Хочешь найти самые выгодные предложения в магазинах? Я помогу тебе найти лучшие скидки. 📉💸');
+});
+
+bot.command("menu", (ctx) => {
+  ctx.reply('Выберите действие:', {
+    reply_markup: {
+      keyboard: [
+        [{ text: "🔍 Функционал" }, { text: "📖 Инструкция" }],
+        [{ text: "🤖 AI-ассистент" }]
+      ],
+      resize_keyboard: true, // делаем клавиатуру компактной
+      one_time_keyboard: false // клавиатура остаётся после нажатия
+    }
+  });
+});
+
+// Обработка нажатий на кнопки
+bot.hears("🔍 Функционал", (ctx) => {
+  ctx.reply("🔍 Тут можно узнать про функционал бота", {
+    reply_markup: { remove_keyboard: true }
+  });
+});
+
+bot.hears("📖 Инструкция", (ctx) => {
+  ctx.reply("📖 Вот инструкция по использованию...", {
+    reply_markup: { remove_keyboard: true }
+  });
+});
+
+bot.hears("🤖 AI-ассистент", (ctx) => {
+  ctx.reply("🤖 AI-ассистент готов помочь!", {
+    reply_markup: { remove_keyboard: true }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+bot.launch(); // запускаем бота
+console.log('бот запущен');
+
+// завершения процесса Ctrl + C в консоли
+process.once('SIGINT', () => {
+  console.log('бот останавливается...');
+  bot.stop('SIGINT');
+});
+
+process.once('SIGTERM', () => {
+  console.log('бот останавливается...');
+  bot.stop('SIGTERM');
 });
