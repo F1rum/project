17b49e8b88cf0c02bbeb42430c83229f862b2bdb
nomercyzzz <ghostnomercy007@gmail.com>
2025-02-26 22:13:00 +0300

    обработка нажатия на кнопку инструкция готова и протестирована

--- bot_development/bot_test.js
+++ bot_development/bot_test.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const { Telegraf } = require('telegraf');
-
+// импортируем модуль command_instruction и присваиваем его переменной
+const command_instruction = require('./command_instruction');
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+
+
 
 bot.help((ctx)=>{
   ctx.reply('Если тебе нужна помощь, напиши мне <b>@nomercyforthemost</b>', { parse_mode: 'HTML' });
@@ -12,32 +16,37 @@ bot.start((ctx) =>{
   ctx.reply('Привет! 👋 Я — твой помощник по скидкам. Хочешь найти самые выгодные предложения в магазинах? Я помогу тебе найти лучшие скидки. 📉💸');
 });
 
+
+
+
 bot.command("menu", (ctx) => {
-  ctx.reply('Выберите действие:', {
+  ctx.reply('<b>✨ Выберите действие:</b>', {
+    parse_mode:"HTML",
     reply_markup: {
       keyboard: [
         [{ text: "🔍 Функционал" }, { text: "📖 Инструкция" }],
         [{ text: "🤖 AI-ассистент" }]
       ],
-      resize_keyboard: true, // делаем клавиатуру компактной
-      one_time_keyboard: false // клавиатура остаётся после нажатия
+      // делаем клавиатуру компактной
+      resize_keyboard: true,
+      // клавиатура остаётся после нажатия 
+      one_time_keyboard: false 
     }
   });
 });
+// вызов функции из импортированного файла 
+command_instruction(bot);
 
-// Обработка нажатий на кнопки
+
+
+
+// обработка нажатия кнопку функционал 
 bot.hears("🔍 Функционал", (ctx) => {
   ctx.reply("🔍 Тут можно узнать про функционал бота", {
     reply_markup: { remove_keyboard: true }
   });
 });
-
-bot.hears("📖 Инструкция", (ctx) => {
-  ctx.reply("📖 Вот инструкция по использованию...", {
-    reply_markup: { remove_keyboard: true }
-  });
-});
-
+// обработка нажатия кнопку аи ассистент 
 bot.hears("🤖 AI-ассистент", (ctx) => {
   ctx.reply("🤖 AI-ассистент готов помочь!", {
     reply_markup: { remove_keyboard: true }
@@ -46,25 +55,19 @@ bot.hears("🤖 AI-ассистент", (ctx) => {
 
 
 
-
-
-
-
-
-
-
-
-
-bot.launch(); // запускаем бота
+// запускаем бота
+bot.launch(); 
 console.log('бот запущен');
 
-// завершения процесса Ctrl + C в консоли
+// завершение процесса Ctrl + C в консоли
 process.once('SIGINT', () => {
   console.log('бот останавливается...');
+  // корректно останавливаем бота
   bot.stop('SIGINT');
 });
 
 process.once('SIGTERM', () => {
   console.log('бот останавливается...');
+  // Корректно останавливаем бота
   bot.stop('SIGTERM');
 });
