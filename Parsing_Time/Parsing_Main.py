4fc2bf59cbc022eae79bf1e368f63c5327d9a9fa
F1rum <qeywtwy3@gmail.com>
2025-03-06 17:23:15 +0300

    питон файл для парсинга В1

---
+++ Parsing_Time/Parsing_Main.py
@@ -0,0 +1,14 @@
+import requests
+st_accept = "text/html" # говорим веб-серверу, что хотим получить html
+# имитируем подключение через браузер Mozilla на macOS
+st_useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_3_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15"
+# формируем хеш заголовков
+headers = {
+   "Accept": st_accept,
+   "User-Agent": st_useragent
+} 
+# отправляем запрос с заголовками по нужному адресу
+req = requests.get("https://5ka.ru/?ysclid=m7xeksrlas220710441")
+# считываем текст HTML-документа
+src = req.text
+print(src)
